3355623d6e8c6596f9d8ce498fc18cf8d48a4ace
chesterma0527 <chesterma0527@gmail.com>
2019-04-27 14:09:00 -0400

    Doggy

---
+++ Doggy/script.js
@@ -0,0 +1,14 @@
+document.getElementById("sunglasses").style.left = "800px"
+document.getElementById("crown").style.left = "800px"
+document.getElementById("crown").style.top = "500px"
+
+
+function moveglasses(){
+	document.getElementById("sunglasses").style.left = "20px"
+	document.getElementById("sunglasses").style.top = "220px"
+}
+
+function movecrown(){
+	document.getElementById("crown").style.left = "130px"
+	document.getElementById("crown").style.top = "120px"
+}
